c327f7d1aa191dabfa8c0df925da4e4b30dd0cd0
gungunzp <flavazp@gmail.com>
2018-09-15 00:02:52 +0300

    fix routes paths, user: create model and add register and login routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://alex:gungun1@ds155492.mlab.com:55492/devconnector-zp'
+  mongoURI: 'mongodb://alex:gungun1@ds155492.mlab.com:55492/devconnector-zp',
+  secretOrKey: 'secret'
 };
